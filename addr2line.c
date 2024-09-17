f77a8625dc6c80bdae51f044396b7a9b96f4f87e
Germán Llort <gllort@bsc.es>
2024-09-17 17:27:58 +0200

    Cleans code and generalizes for binutils & elfutils

--- addr2line.c
+++ addr2line.c
@@ -4,100 +4,136 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define READ_END 0
-#define WRITE_END  1
+#define UNRESOLVED_ADDRESS "??"
 
-// Pipe file descriptors for communication between parent and child parent
+enum {
+	READ_END = 0,
+	WRITE_END = 1
+};
 
-int parent_write[2];		//Parent wrtes to child
-int child_write[2];			//Child writes to parent
-FILE *addr2line_fd = NULL;	// File descriptor for reading addr2line output
+// File descriptor for reading addr2line output
+FILE *addr2line_fd = NULL;	
 
+// Pipes for communication between parent and child processes
+int parent_write[2];		// Parent writes to child
+int child_write[2];			// Child writes to parent
+
+// Flag to indicate whether to use elfutils or binutils
 int use_elfutils = 0;
-void check_toolset_preference() {
+
+/**
+ * check_toolset
+ * 
+ * Check the environment variable USE_ELFUTILS to determine whether to use elfutils or binutils (default).
+ */
+void select_backend() {
     char *env = getenv("USE_ELFUTILS");
-    if (env && strcmp(env, "1") == 0) {
+    if ((env != NULL) && (strcmp(env, "1") == 0)) 
+	{
         use_elfutils = 1;
     }
 }
+
+static int is_binary_file(const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        perror("Unable to open file");
+        return -1;
+    }
+
+    unsigned char buffer[512]; 
+    size_t bytes_read = fread(buffer, 1, sizeof(buffer), file);
+    fclose(file);
+
+    for (size_t i = 0; i < bytes_read; i++) {
+        // If we find a non-printable and non-whitespace character, it's likely a binary file
+        if (!isprint(buffer[i]) && !isspace(buffer[i])) {
+            return 1; // Binary file
+        }
+    }
+    return 0; // Text file
+}
+
 /** 
- * Initialize the addr2line provess with the given maps file.
+ * addr2line_init
+ * 
+ * Initialize the addr2line process with the given maps file.
  * This function sets up pipes for inter-process communication, forks a child
  * process to execute addr2line, and connects the pipes to the child's stdin and stdout.
  * 
- * @param maps		Path to the maps file for addr2line.
+ * @param filename Path either to the binary or to a dump of the /proc/self/maps.
  */
-
-//void addr2line_init(char *binary)
-void addr2line_init(char *maps)
+void addr2line_init(char *filename)
 {
-	check_toolset_preference();
-	char *addr2line_started = getenv("ADDR2LINE_STARTED");
+	select_backend();
 
-	//If addr2line is already started return early
-	if (addr2line_started)
+	// Check if the filename is a binary file or a maps file
+	int is_binary = is_binary_file(filename);
+	if (!use_elfutils && !is_binary)
 	{
-	    return;
+		fprintf(stderr, "ERROR: addr2line_init: binutils backend can only be used with binary files\n");
+		exit(EXIT_FAILURE);	
 	}
-	else
-	{
-		fprintf(stderr, "%s : Initializing addr2line\n", __func__);
 
-		//Creating pipes for communication between parent and child processes
-		if(pipe(parent_write) == -1 || pipe(child_write) == -1)
+	// Hack to prevent recursive initialization when addr2line_init is called from a library constructor, and the forked child process triggers the constructor again
+	char *addr2line_started = getenv("ADDR2LINE_STARTED");
+	if (addr2line_started) return;
+	putenv("ADDR2LINE_STARTED=1");
+
+	// Creating pipes for communication between parent and child processes
+	if (pipe(parent_write) == -1 || pipe(child_write) == -1)
+	{
+		perror("Failed to create pipes");
+		exit(EXIT_FAILURE);
+	}
+
+	// Fork a child process to run addr2line as a continuous background process
+	if (fork() == 0)
+	{
+		// In the child process
+		close(parent_write[WRITE_END]); // Close unused 'write end' of the parent_write pipe
+		close(child_write[READ_END]);   // Close unused 'read end' of the child_write pipe
+
+		if ((dup2(parent_write[READ_END], STDIN_FILENO) == -1) || // Get stdin to read from from parent_write[READ_END] pipe
+			(dup2(child_write[WRITE_END], STDOUT_FILENO) == -1))  // Redirect stdout to write to child_write[WRITE_END] pipe
 		{
-			perror("Failed to create pipes");
+			perror("Failed to duplicate file descriptor");
 			exit(EXIT_FAILURE);
 		}
+		
+		// Close the duplicated file descriptors
+		close(parent_write[READ_END]);
+		close(child_write[WRITE_END]);
 
-		//Fork a child process
-		if (fork() == 0)
-		{
-			//In the child process
-			close(parent_write[WRITE_END]);				// Close unused 'write end' of the parent_write pipe
-			close(child_write[READ_END]);				// Close unused 'read end' of the child_write pipe
+		// Set up arguments for execution
+		char *backend = use_elfutils ? "eu-addr2line" : "addr2line";
+		char *argv_elfutils[] = { backend, (is_binary ? "-e" : "-M"), filename, "-C", "-f", "-i", NULL };
+		char *argv_binutils[] = { backend, "-e", filename, "-C", "-f", NULL };
+		char **argv = (use_elfutils ? argv_elfutils : argv_binutils); 
 
-			
-			if(dup2(parent_write[READ_END], STDIN_FILENO) == -1 ||			//Get stdin to read from from parent_write[READ_END] pipe
-				dup2(child_write[WRITE_END], STDOUT_FILENO) == -1)		//Redirect stdout to write to child_write[WRITE_END] pipe
-			{
-				perror("Failed to duplicate file descriptor");
-				exit(EXIT_FAILURE);
-			}
-			
-			//Close the duplicated file descriptors
-			close(parent_write[READ_END]);
-			close(child_write[WRITE_END]);
+		// Replaces the current process with addr2line
+		execvp(backend, argv);
+	}
+	else 
+	{
+		// In the parent process
+		close(parent_write[READ_END]); // Close unused 'read end' of the parent_write pipe
+		close(child_write[WRITE_END]); // Close unused 'write end' of the child_write pipe
 
-			
-			char *tool = use_elfutils ? "/home/snagarka/Projects/elfutils/bin/eu-addr2line" : "/home/snagarka/Projects/binutils/bin/addr2line";
-			//Set up arguments for execution
-			char *argv[] = { tool, "-M", maps, "-f", "-C", NULL };
-
-			//Set environment variables to indicate addr2line has started
-			putenv("ADDR2LINE_STARTED=1");
-
-			//Replaces the current process with addr2line
-			execvp(tool, argv);
-		}
-		else 
-		{
-			//In the parent process
-			close(parent_write[READ_END]);		// Close unused 'read end' of the parent_write pipe
-			close(child_write[WRITE_END]);		// Close unused 'write end' of the child_write pipe
-
-			// Open a FILE stream to read the child's output
-			addr2line_fd = fdopen(child_write[READ_END], "r");
-			if (addr2line_fd == NULL) {
-				perror("fdopen failed");
-				exit(EXIT_FAILURE);
-			}
+		// Open a FILE stream to read addr2line's output
+		addr2line_fd = fdopen(child_write[READ_END], "r");
+		if (addr2line_fd == NULL) {
+			perror("fdopen failed");
+			exit(EXIT_FAILURE);
 		}
 	}
 }
 
 /**
+ * addr2line_translate
+ * 
  * Translate a memory address into the corresponding function, file, line, and column
  * using addr2line
  * 
@@ -105,86 +141,49 @@ void addr2line_init(char *maps)
  * @param function 	Output buffer for the function name.
  * @param file 		Output buffer for the filename.
  * @param line		Output pointer for the line number.
- * @param col		Output pointer for the column number.
+ * @param column	Output pointer for the column number.
  */
-
-void addr2line_translate(char *str, char *function, char *file, int *line, int *col)
+void addr2line_translate(char *address, char **function, char **file, int *line, int *column)
 {
 	char buf[BUFSIZ];
 
-	// Write the address to the addr2line process via the pipe.
-	sprintf(buf, "%p\n", str); // Should this be p or s?
-	
-	fprintf(stderr, "%s : Writing %s to PIPE\n", __func__, buf);
+	// Format the address pointer as a string and pass it to addr2line
+	sprintf(buf, "%p\n", address); 
 	write(parent_write[WRITE_END], buf, strlen(buf));
 
 	// Read the function name from addr2line's output
 	fgets(buf, sizeof(buf), addr2line_fd);
-	fprintf(stderr, "\tfirst fgets is  %s", buf);
 	if (buf[strlen(buf)-1] == '\n') {
-		strncpy(function, buf, strlen(buf)-1);		// Remove the newline character
+		buf[strlen(buf)-1] = '\0'; // Remove the newline character
+		*function = strdup(buf);
 	} else {
-		function = "??";
+		*function = strdup(UNRESOLVED_ADDRESS);
 	}
 
 	// Read the filename, line number, and column number from addr2line's output
 	fgets(buf, sizeof(buf), addr2line_fd);
-	fprintf(stderr, "\tsecond fgets is  %s", buf);
 
-	if (use_elfutils) {	
-
+	*line = *column = 0;
+	if (use_elfutils) 
+	{	
 		// Parsing the column number
-		char *last_colon = rindex(buf, ':'); 
-		*col = atoi(last_colon + 1);
-		*last_colon = '\0'; 
-
-		// Parsing the line number
-		char *first_colon = rindex(buf, ':');
-		*line = atoi(first_colon + 1);
-		*first_colon = '\0'; 
-
-		// Copying the filename
-		if (strlen(buf) < BUFSIZ) { 
-			strncpy(file, buf, BUFSIZ);
-			file[BUFSIZ - 1] = '\0'; 
+		char *last_colon = strrchr(buf, ':'); 
+		if (last_colon != NULL)
+		{
+			*column = atoi(last_colon + 1);
+			*last_colon = '\0'; 
 		}
-		fprintf(stderr, "function: %s file: %s line: %d col: %d\n", function, file, *line, *col);
-
 	}
 
-	#if 0
-		if (buf[strlen(buf)-1] == '\n') {
-			// Do exhaustive error checking in parsing, use explode? sscanf?
-			char *line_str = index(buf, ':');
-			*line = atoi(line_str + 1);
-			line_str = index(line_str + 1, ':');
-			*col = atoi(line_str + 1);
-			*line_str = '\0';
-			strcpy(file, buf);
-		} else {
-			file = "??";
-			*line = 0;
-			*col = 0;
-		}
+	// Parsing the line number
+	char *first_colon = strrchr(buf, ':');
+	if (first_colon != NULL)
+	{
+		*line = atoi(first_colon + 1);
+		*first_colon = '\0'; 
+	}
+	// Copying the filename
+	*file = strdup(buf);
 
-		fprintf(stderr, "function: %s file: %s line: %d col: %d\n", function, file, *line, *col);
-	#endif
-}
-
-int main(int argc, char **argv)
-{
-	char function[BUFSIZ];
-	char file[BUFSIZ];
-	int line, col;
-
-	//addr2line_init("/apps/GPP/BSCTOOLS/extrae/latest/openmpi_4_1_5/lib/libmpitrace.so");
-
-	//Initialize addr2line with the current process's memory map
-	addr2line_init("/proc/self/maps");
-
-	//Translate addresses to readable format
-	addr2line_translate("00000000000cc9b0", function, file, &line, &col);
-	addr2line_translate("0000000000042ff0", function, file, &line, &col);
-
-	return 0;
+	fprintf(stderr, "function: %s file: %s line: %d column: %d\n", *function, *file, *line, *column);
 }
--- mpi/run.sh
+++ mpi/run.sh
@@ -2,4 +2,6 @@
 
 export LD_LIBRARY_PATH=..:$LD_LIBRARY_PATH
 
-mpirun -n 1 ./trace.sh ./app
+export USE_ELFUTILS=1
+
+$HOME/Apps/openmpi/latest/bin/mpirun -n 1 ./trace.sh ./app

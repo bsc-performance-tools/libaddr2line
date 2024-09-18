e5275f3d14fe9373c2e4b3507a32ceccb9ede77d
Germán Llort <gllort@bsc.es>
2024-09-18 19:11:44 +0200

    Adds option to keep unresolved addresses in the output instead of "??"

--- addr2line.c
+++ addr2line.c
@@ -17,6 +17,9 @@ int childWrite[2];  // Child writes to parent
 // User can override the default backend through the environment variable LIBADDR2LINE_BACKEND
 int useBackend = DEFAULT_BACKEND;
 
+// Selected configuration options
+int setOptions = 0;
+
 /**
  * select_backend
  * 
@@ -40,6 +43,13 @@ static void select_backend()
 	}
 }
 
+/**
+ * is_binary_file
+ * 
+ * Check if the given file is a binary file by reading the first 512 bytes and checking for non-printable characters.
+ * 
+ * @param filename Path to the file to check.
+ */
 static int is_binary_file(const char *filename) {
     FILE *file = fopen(filename, "rb");
     if (!file) {
@@ -60,6 +70,11 @@ static int is_binary_file(const char *filename) {
     return 0; // Text file
 }
 
+/**
+ * write_with_retry
+ * 
+ * Safe write wrapper that retries the write operation until all data is written. 
+ */
 static ssize_t write_with_retry(int fd, const void *buf, size_t count) {
     ssize_t written = 0, result = 0;
 
@@ -92,12 +107,15 @@ static ssize_t write_with_retry(int fd, const void *buf, size_t count) {
  * 
  * @param filename Path either to the binary or to a dump of the /proc/self/maps.
  */
-void addr2line_init(char *filename)
+void addr2line_init(char *filename, int options)
 {
 	// Hack to prevent recursive initialization when addr2line_init is called from a library constructor, and the forked child process triggers the constructor again
 	char *addr2line_started = getenv("LIBADDR2LINE_STARTED");
 	if (addr2line_started) return;
 	putenv("LIBADDR2LINE_STARTED=1");
+
+	// Set the configuration options
+	setOptions = options;
 
 	// Check the backend to use
 	select_backend();
@@ -183,13 +201,14 @@ void addr2line_init(char *filename)
  * @param line		Output pointer for the line number.
  * @param column	Output pointer for the column number.
  */
-void addr2line_translate(char *address, char **function, char **file, int *line, int *column)
+void addr2line_translate(void *address, char **function, char **file, int *line, int *column)
 {
+	char address_str[BUFSIZ];
 	char buf[BUFSIZ];
 
 	// Format the address pointer as a string and pass it to addr2line
-	sprintf(buf, "%p\n", address); 
-	write_with_retry(parentWrite[WRITE_END], buf, strlen(buf));
+	sprintf(address_str, "%p\n", address); 
+	write_with_retry(parentWrite[WRITE_END], address_str, strlen(address_str));
 
 	// Read the function name from addr2line's output
 	*function = NULL;
@@ -198,8 +217,10 @@ void addr2line_translate(char *address, char **function, char **file, int *line,
 		if (buf[strlen(buf)-1] == '\n') {
 			buf[strlen(buf)-1] = '\0'; // Remove the newline character
 		} 
-		// Copying the function name
-		*function = strdup(buf);
+		// Copying the function name only if has been translated
+		if (strcmp(buf, UNKNOWN_ADDRESS) != 0) {
+			*function = strdup(buf);
+		}
 	}
 
 	// Read the filename, line number, and column number from addr2line's output
@@ -226,11 +247,13 @@ void addr2line_translate(char *address, char **function, char **file, int *line,
 			*line = atoi(first_colon + 1);
 			*first_colon = '\0'; 
 		}
-		// Copying the filename
-		*file = strdup(buf);
+		// Copying the filename only if has been translated
+		if (strcmp(buf, UNKNOWN_ADDRESS) != 0) {
+			*file = strdup(buf);
+		}
 	}
 
-	// Alternatively, we could return the address string (strdup(address)) if addr2line fails to resolve 
-	if (*function == NULL) *function = strdup(UNKNOWN_ADDRESS);
-	if (*file == NULL) *file = strdup(UNKNOWN_ADDRESS);
+	// Make sure we return something for function and file when the translation fails
+	if (*function == NULL) *function = strdup((setOptions & OPTION_KEEP_UNRESOLVED_ADDRESSES) ? address_str : UNKNOWN_ADDRESS);
+	if (*file == NULL) *file = strdup((setOptions & OPTION_KEEP_UNRESOLVED_ADDRESSES) ? address_str : UNKNOWN_ADDRESS);
 }
--- addr2line.h
+++ addr2line.h
@@ -4,6 +4,9 @@
 
 #define UNKNOWN_ADDRESS "??"
 
+// Available configuration options
+#define OPTION_KEEP_UNRESOLVED_ADDRESSES (1 << 0) // Keep the unresolved addresses in the output instead of "??"
+
 // Available addr2line backends
 enum {
 #if defined(HAVE_ELFUTILS)
@@ -28,3 +31,7 @@ enum {
 	READ_END = 0,
 	WRITE_END = 1
 };
+
+// Function prototypes
+void addr2line_init(char *filename, int options);
+void addr2line_translate(void *address, char **function, char **file, int *line, int *column);

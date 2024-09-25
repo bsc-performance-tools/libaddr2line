9f42cab8a6387e44c43637c96e702af0eb47bac6
Germán Llort <gllort@bsc.es>
2024-09-25 17:17:09 +0200

    Moves definitions from .h to .c to avoid references to config.h from the public header

--- addr2line.c
+++ addr2line.c
@@ -6,6 +6,34 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "addr2line.h"
+#include "config.h"
+
+#define UNKNOWN_ADDRESS "??"
+
+// Available addr2line backends
+enum {
+#if defined(HAVE_ELFUTILS)
+	USE_ELFUTILS,
+#endif
+#if defined(HAVE_BINUTILS)
+	USE_BINUTILS,
+#endif
+	NUM_AVAILABLE_BACKENDS
+};
+
+// Default addr2line backend
+#if defined(HAVE_ELFUTILS)
+# define DEFAULT_BACKEND USE_ELFUTILS
+#elif defined(HAVE_BINUTILS)
+# define DEFAULT_BACKEND USE_BINUTILS
+#else
+# error "No addr2line backend available"
+#endif
+
+enum {
+	READ_END = 0,
+	WRITE_END = 1
+};
 
 // File descriptor for reading addr2line output
 FILE *addr2lineOutputStream = NULL;	
--- addr2line.h
+++ addr2line.h
@@ -1,37 +1,8 @@
 #pragma once
 
-#include "config.h"
-
-#define UNKNOWN_ADDRESS "??"
-
 // Available configuration options
 #define OPTION_KEEP_UNRESOLVED_ADDRESSES (1 << 0) // Keep the unresolved addresses in the output instead of "??"
-
-// Available addr2line backends
-enum {
-#if defined(HAVE_ELFUTILS)
-	USE_ELFUTILS,
-#endif
-#if defined(HAVE_BINUTILS)
-	USE_BINUTILS,
-#endif
-	NUM_AVAILABLE_BACKENDS
-};
-
-// Default addr2line backend
-#if defined(HAVE_ELFUTILS)
-# define DEFAULT_BACKEND USE_ELFUTILS
-#elif defined(HAVE_BINUTILS)
-# define DEFAULT_BACKEND USE_BINUTILS
-#else
-# error "No addr2line backend available"
-#endif
-
-enum {
-	READ_END = 0,
-	WRITE_END = 1
-};
-
+										  
 // Function prototypes
 void addr2line_init(char *filename, int options);
 void addr2line_translate(void *address, char **function, char **file, int *line, int *column);

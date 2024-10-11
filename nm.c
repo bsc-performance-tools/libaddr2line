47c95a3ec964bf512a0fd4a43da299811f761ca1
Germán Llort <gllort@bsc.es>
2024-10-11 15:53:17 +0200

    Adds filters to exclude non-data objects and zero-sized symbols from the symtab dump

--- nm.c
+++ nm.c
@@ -8,7 +8,8 @@
 #include <stdlib.h>
 #include "nm.h"
 
-#define SKIP_UNDEFINED_SYMBOLS // Define this to exclude undefined symbols from the symtab dump
+#define FILTER_DATA_OBJECTS     // Define this to exclude non-data objects from the symtab dump
+#define SKIP_ZERO_SIZED_SYMBOLS // Define this to exclude zero-sized objects from the symtab dump
 
 int nm_symtab_dump(char *binary_path, symtab_t *symtab)
 {
@@ -40,9 +41,34 @@ int nm_symtab_dump(char *binary_path, symtab_t *symtab)
     for (i = 0; i < count; ++i) {
         GElf_Sym sym;
         gelf_getsym(data, i, &sym);
-#if defined(SKIP_UNDEFINED_SYMBOLS) // Skips undefined symbols, forward declarations, weak symbols, etc.
-        if ((sym.st_value) && (sym.st_size))
+
+        // Exclude non-data objects and zero-sized symbols
+        int filter = 0;
+#if defined(FILTER_DATA_OBJECTS)
+#if 0
+        /* Legal values for ST_TYPE subfield of st_info (symbol type). See elf.h from elfutils for details. */
+
+        #define STT_NOTYPE      0               /* Symbol type is unspecified */
+        #define STT_OBJECT      1               /* Symbol is a data object */
+        #define STT_FUNC        2               /* Symbol is a code object */
+        #define STT_SECTION     3               /* Symbol associated with a section */
+        #define STT_FILE        4               /* Symbol's name is file name */
+        #define STT_COMMON      5               /* Symbol is a common data object */
+        #define STT_TLS         6               /* Symbol is thread-local data object*/
+        #define STT_NUM         7               /* Number of defined types.  */
+        #define STT_LOOS        10              /* Start of OS-specific */
+        #define STT_GNU_IFUNC   10              /* Symbol is indirect code object */
+        #define STT_HIOS        12              /* End of OS-specific */
+        #define STT_LOPROC      13              /* Start of processor-specific */
+        #define STT_HIPROC      15              /* End of processor-specific */
 #endif
+        int sym_type = GELF_ST_TYPE(sym.st_info);
+        if ((sym_type != STT_OBJECT) && (sym_type != STT_COMMON) && (sym_type != STT_TLS)) filter = 1;
+#endif
+#if defined(SKIP_ZERO_SIZED_SYMBOLS)
+        if ((!sym.st_value) || (!sym.st_size)) filter = 1;
+#endif
+        if (!filter)
         {
             entries[ii].name = elf_strptr(elf, shdr.sh_link, sym.st_name);
             entries[ii].start = sym.st_value;
@@ -52,7 +78,7 @@ int nm_symtab_dump(char *binary_path, symtab_t *symtab)
         }
     }
 
-    /* Print the symbol names and addresses */
+    // Print the unfiltered symbol names and addresses
     for (i = 0; i < ii; ++i) {
         printf("%s [0x%lx-0x%lx]\n", entries[i].name, entries[i].start, entries[i].end + entries[i].size); 
     } 
